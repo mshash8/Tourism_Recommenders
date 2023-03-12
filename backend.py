fd9c0ab1a636be0e477b489816ecbd72f4fd5abf
Tanushree Yandra <tyandra@uw.edu>
2023-03-11 16:13:14 -0800

    Updated frontend and backend code

--- backend.py
+++ backend.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import math
 import requests, json
 import base64
 from datetime import datetime,timedelta
@@ -7,10 +9,24 @@ import geocoder
 from geopy.geocoders import Nominatim
 import const
 
-def call_google_api(url, query, api_key, fields):
-    req = requests.get(url + 'query=' + query + '&fields=' + fields + '&key=' + api_key)
-    req_json = req.json()
-    return req_json.get('results')
+def call_google_api(url, queries, api_key, fields, latitude, longitude, r, b):
+    places_df = pd.DataFrame()
+    location_df = pd.DataFrame([[latitude,longitude]], columns = ['Lat','Long'])
+    for query in queries:
+        req = requests.get(url + 'query=' + query + '&fields=' + fields + '&key=' + api_key)
+        req_json = req.json()
+        api_response = req_json.get('results')
+        df = pd.json_normalize(api_response)
+        lat_long_df = pd.DataFrame(np.repeat(location_df.values, len(df.index), axis=0))
+        lat_long_df.columns = location_df.columns
+        df['Distance'] = haversine(lat_long_df['Lat'], lat_long_df['Long'], df['geometry.location.lat'], df['geometry.location.lng'])
+        df['Query'] = query
+        places_df = pd.concat([places_df,df])
+    
+    if places_df.empty == False:
+        places_df = clean_and_sort(places_df, r, b)
+    
+    return places_df
 
 def call_meteostat_api(latitude, longitude, date, time):
     stations = Stations()
@@ -24,6 +40,8 @@ def call_meteostat_api(latitude, longitude, date, time):
     data = data.fetch()
     weather = list(data['coco'].values)
     unique  = list(set(weather))
+    if 'nan' in unique:
+        unique.remove('nan')
     return max(unique)
 
 def decode_api_key(encoded_key):
@@ -57,15 +75,55 @@ def get_current_loc():
 
 
 def basic_rules(defining_weather, location):
+    if math.isnan(defining_weather) == True:
+        alt_query = 'Tourist Places in' + str(location) 
+        return alt_query
     defining_weather = int(defining_weather)
     location = str(location)
     weather_rules = const.WEATHER_CODES
     if defining_weather <= 2:
-        queries = ['Outdoor activities near %s' % location, 'Parks near %s' % location, 
-        'Beaches near %s' % location, 'Hikes or Trails near %s' % location]
-        alt_query = 'Things to do near' + location + 'when it is' + weather_rules[str(defining_weather)]
-        query = 'Outdoor activities in Seattle'
+        queries = ['Parks in %s' % location, 'Beaches in %s' % location, 
+        'Hikes in %s' % location, 'Trails in %s' % location,
+        'Botanical Gardens in %s' % location, 'Outdoor activities in %s' % location]
     elif defining_weather <= 5:
-        alt_query = 'Things to do near' + location + 'when it is' + weather_rules[str(defining_weather)]
-    alt_query = 'Things to do near' + location + 'when it is' + weather_rules[str(defining_weather)]
-    return alt_query
+        queries = ['Restaurants in %s' % location, 'Cafes in %s' % location, 
+        'Hikes in %s' % location, 'Trails in %s' % location,
+        'Museums in %s' % location, 'Indoor activities in %s' % location]
+    elif defining_weather <= 11 or defining_weather == 17 or defining_weather == 18:
+        queries = ['Restaurants in %s' % location, 'Cafes in %s' % location,
+        'Museums in %s' % location, 'Indoor activities in %s' % location]
+    elif defining_weather == 14 or defining_weather == 15 or defining_weather == 21:
+        queries = ['Restaurants in %s' % location, 'Cafes in %s' % location, 
+        'Snow hikes in %s' % location, 'Snow trails in %s' % location,
+        'Skiing spots in %s' % location, 'Museums in %s' % location, 
+        'Indoor activities in %s' % location]
+    else:
+        queries = []
+    return queries
+
+def haversine(lat1, lon1, lat2, lon2, to_radians=True, earth_radius=6378):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees or in radians)
+
+    All (lat, lon) coordinates must have numeric dtypes and be of equal length.
+    
+    Returns distance in kms.
+
+    """
+    if to_radians:
+        lat1, lon1, lat2, lon2 = np.radians([lat1, lon1, lat2, lon2])
+    a = np.sin((lat2-lat1)/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin((lon2-lon1)/2.0)**2
+
+    return earth_radius * 2 * np.arcsin(np.sqrt(a))
+
+def clean_and_sort(df, r, b):
+    df = df.drop_duplicates(subset = ['name', 'formatted_address'])
+    df['Distance'] = df['Distance'].astype(int)
+    df = df.loc[(df['Distance'] <=20) & (df['rating']>=r)]
+    high_price_index = df[(df['price_level'] > b)].index
+    df.drop(high_price_index , inplace=True)
+    
+    df.sort_values(['rating','Distance','user_ratings_total'], ascending = [False, True, False], inplace=True, na_position="last")
+    return df
+    
--- frontend.py
+++ frontend.py
@@ -1,34 +1,66 @@
 import streamlit as st
 import pandas as pd
+import math
 import const
 import backend
 
-r = st.number_input("Minimum rating you want 1-5")
-b = st.number_input("Price level 1-5")
-date = st.date_input("When do you wish to visit (only upto 1 day beyond today")
-time = st.time_input("What time do you wish to visit")
-location = st.text_input("Enter your location or leave blank to use your current location")
+st.set_page_config(layout = "wide")
 
-api_key = backend.decode_api_key(const.ENCODED_GOOGLE_API_KEY)
-if location == "":
-    location = backend.get_user_entered_loc(location)
+var1 = st.empty()
+var2 = st.empty()
+var3 = st.empty()
+var4 = st.empty()
+var5 = st.empty()
+var6 = st.empty()
+var7 = st.empty()
 
-st.write(location)
+pref = var1.selectbox('Do you have any particular preference for a place?', 
+        ('None', 'Beaches', 'Cafes and Restaurants', 'Hikes and Trails',
+         'Indoor Activities', 'Museums', 'Outdoor Activities',
+         'Parks & Botanical Gardens', 'Skiing Spots'))
+date = var2.date_input("When do you wish to visit? (To get more accurate results, please enter a date that is not beyond 2-3 days from today)\nNOTE: Dates beyond 8 days from today are invalid!")
+time = var3.time_input("What time do you wish to visit?")
+r = var4.number_input("Minimum rating you want from 1-5", min_value=0.0, max_value=5.0, value = 3.5, step=0.1)
+b = var5.number_input("Maximum price level (from 1-5 with 1 being the lowest and 5 being the highest)", min_value=1, max_value=5, step=1)
+location = var6.text_input("Enter your location or leave blank to use your current location")
+enter_button = var7.button(label='Enter!')
 
-latitude, longitude = backend.get_latitude_and_longitude(location)
-meteostat_api_response = backend.call_meteostat_api(latitude,longitude,date,time)
-st.write(meteostat_api_response)
+if enter_button: 
+    api_key = backend.decode_api_key(const.ENCODED_GOOGLE_API_KEY)
+    if location == "":
+        location = backend.get_user_entered_loc(location)
+            
+    latitude, longitude = backend.get_latitude_and_longitude(location)
+    meteostat_api_response = backend.call_meteostat_api(latitude,longitude,date,time)
+            
+    queries = backend.basic_rules(meteostat_api_response, location)
+    google_api_response = backend.call_google_api(const.GOOGLE_MAPS_API, queries, api_key, 'opening_hours', latitude, longitude, r, b)
 
+    var1.empty()
+    var2.empty()
+    var3.empty()
+    var4.empty()
+    var5.empty()
+    var6.empty()
+    var7.empty()
 
-query = backend.basic_rules(meteostat_api_response, location)
-google_api_response = backend.call_google_api(const.GOOGLE_MAPS_API, query, api_key, 'opening_hours')
-st.write(google_api_response)
+    if google_api_response.empty:
+        st.header('Oops! Looks like the weather is not favourable at your desired location :(')
+    else:
+        if math.isnan(meteostat_api_response) == True:
+            st.header('The weather for your desired date is uncertain')
+        if meteostat_api_response <= 2:
+            st.header('The weather for your desired date is sunny :sun_with_face:')
+        elif meteostat_api_response <= 5:
+            st.header('The weather for your desired date is cloudy :cloud:')
+        elif meteostat_api_response <= 11 or meteostat_api_response == 17 or meteostat_api_response == 18:
+            st.header('The weather for your desired date is rainy :rain_cloud:')
+        elif meteostat_api_response == 14 or meteostat_api_response == 15 or meteostat_api_response == 21:
+            st.header('The weather for your desired date is snowy :snowman:')
 
+        st.subheader('Here are our recommendations!')
+        for query in queries:
+            st.subheader(query.split(' in')[0])
+            df = google_api_response[google_api_response['Query']==query]
+            st.write(df)
 
-## ST.WRITE TO THE FRONT END HERE
-
-# loc = st.text_input("Enter your location or leave blank to use your current location")
-
-# res = backend.user_entered_loc(loc)
-
-# st.write(res)
