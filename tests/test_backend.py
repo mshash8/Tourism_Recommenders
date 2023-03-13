b25ee76ccb107c635bc8807d9a9ecb1d386c2d57
TanushreeYandra <121986069+TanushreeYandra@users.noreply.github.com>
2023-03-13 10:44:21 -0700

    Update test_backend.py

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -44,33 +44,43 @@ class TestBackend(unittest.TestCase):
         query = basic_rules(defining_weather, location)
         self.assertTrue(True) 
 
-    #test to see whether exception is raised when date given is BEFORE current date
-    def test_correct_date(self):
-        latitude = 47.6062
-        longitude = 122.3321
+    #Edge Tests
+    def test_edge_correct_date(self):
+        """
+        This function performs an edge test to check how the program
+        works when one goes "off script". This test accounts for the
+        case where the entered date and time belongs to a past date or
+        the entered date is beyond 8 days from the current date.
+
+        """
         date = "2023-03-10" 
         time = "17:02:00"
         with self.assertRaises(ValueError):
-            call_meteostat_api(latitude, longitude, date, time)
+            check_date_invalid(date time)
 
-    #test to see whether missing data is taken care of
-    def test_for_missing_data(self):
-        url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
-        query = "Tourist places to visit in Seattle" ##PUT MISSING DATA QUERY
-        api_key = 'AIzaSyDvXY6zrSDLbP_uNCc7UV1zWqobtkk0l7A'
-        fields = 'opening_hours,price'
-        google_api_response = call_google_api(url, query, api_key, fields)
-        self.assertTrue(True) 
+    def test_edge_address_validity(self):
+        """
+        This function performs an edge test to check how the program
+        works when one goes "off script". This test accounts for the
+        case where the entered address is invalid and does not exist
+        in the google maps api dataset.
 
-    def test_address_validity(self):
+        """
         location_as_string = "Csdgfsdg"
         with self.assertRaises(ValueError):
-            loc = get_user_entered_loc(location_as_string)
+            check_address_invalid(location_as_string)
 
-    def test_address_char_validity(self):
+    def test_edge_address_char_validity(self):
+        """
+        This function performs an edge test to check how the program
+        works when one goes "off script". This test accounts for the
+        case where the entered address has invalid characters i.e.
+        anything other than english letter, digits, or a comma.
+        
+        """
         location_as_string = "2$ Capitol Hill^"
         with self.assertRaises(ValueError):
-            loc = get_user_entered_loc(location_as_string)
+            check_address_characters(location_as_string)
 
 if __name__ == "__main__":
    unittest.main()
