dfd2469cae240bce3eb61d518fcf3d5e0942e68d
anishdixit-uw <122193072+anishdixit-uw@users.noreply.github.com>
2023-03-14 11:18:00 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='Tourism_Recommenders',
       version='1.0',
       description=' An app for Tourism Recommendations',
       author='Mithali,Tanushree,Ananya,Anish',
-      author_email='mshash@uw.edu,anair@uw.edu,tyandra@uw.edu',
+      author_email='mshash@uw.edu,anair@uw.edu,tyandra@uw.edu,andixit@uw.edu',
       packages=['Tourism_Recommenders'],
      )
