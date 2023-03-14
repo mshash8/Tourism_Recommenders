6dde0866a152fc6fc6443ae37c2a5eb80f5bb427
Ananya Nair <ananyanair@Ananyas-MacBook-Pro-3.local>
2023-03-13 21:37:08 -0700

    Adjusted tests

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,47 +1,75 @@
+'''
+This is the unit test file for the code.
+'''
 import unittest
-from Tourism_Recommenders.backend import *
+from recommender_engine.backend import call_google_api, call_meteostat_api, decode_api_key, \
+get_latitude_and_longitude, get_user_entered_loc, basic_rules, check_date_invalid, \
+check_address_invalid, check_address_characters, haversine
+import const
 
 
 class TestBackend(unittest.TestCase):
-    def test_call_google_api_smoke_test(self):
+    '''
+    Class to conduct unit tests for the backend functions
+    '''
+    def test_call_google_api(self):
+        '''
+        tests the working of the call_google_api function
+        '''
         url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
         query = "Tourist places to visit in Seattle"
         api_key = 'AIzaSyDvXY6zrSDLbP_uNCc7UV1zWqobtkk0l7A'
         fields = 'opening_hours'
-        google_api_response = call_google_api(url, query, api_key, fields)
+        lat = 47.6062
+        longi = 122.3321
+        rating = 3
+        price = 2
+        call_google_api(url, query, api_key, fields, lat, longi, rating, price)
         self.assertTrue(True)
 
+
     def test_call_meteostat_api(self):
+        '''
+        tests the working of the call_meteostat_api function
+        '''
         latitude = 47.6062
         longitude = 122.3321
-        date = "2023-03-12" 
+        date = "2023-03-12"
         time = "17:02:00"
-        meteostat_response = call_meteostat_api(latitude, longitude, date, time)
+        call_meteostat_api(latitude, longitude, date, time)
         self.assertTrue(True)
 
     def test_decode_api_key(self):
-        api_key = const.ENCODED_GOOGLE_API_KEY 
-        decoded_key = decode_api_key(api_key)
-        self.assertTrue(True) 
+         '''
+        tests the working of the decode_api_key function
+        '''
+        api_key = const.ENCODED_GOOGLE_API_KEY
+        decode_api_key(api_key)
+        self.assertTrue(True)
+
 
     def test_get_latitude_and_longitude(self):
+        '''
+        tests the working of the get_latitude_and_longitude function
+        '''
         location = "Seattle"
         get_latitude_and_longitude(location)
         self.assertTrue(True)
 
-    def test_get_user_entered_loc(self):
-        location_as_string = "Capitol Hill"
-        loc = get_user_entered_loc(location_as_string)
+     def test_get_user_entered_loc(self):
+        '''
+        tests the working of the get_user_entered_loc function
+        '''
+        get_user_entered_loc()
         self.assertTrue(True)
 
-    def test_get_current_loc(self):
-        loc = get_current_loc()
-        self.assertTrue(True) 
-
     def test_basic_rules(self):
+        '''
+        tests the working of the basic_rules function
+        '''
         defining_weather = 2
         location = "Seattle"
-        query = basic_rules(defining_weather, location)
+        basic_rules(defining_weather, location)
         self.assertTrue(True) 
 
     #Edge Tests
@@ -81,6 +109,17 @@ class TestBackend(unittest.TestCase):
         location_as_string = "2$ Capitol Hill^"
         with self.assertRaises(ValueError):
             check_address_characters(location_as_string)
+    
+    def check_haversine_fn(self):
+        '''
+        Should return 0 as both the locations are the same.
+        '''
+        lat1 = 47.6062
+        long1 = 122.3321
+        lat2 = 47.6062
+        long2 = 122.3321
+        dist = haversine(lat1, long1, lat2, long2)
+        self.assertAlmostEqual(dist, 0)
 
 if __name__ == "__main__":
    unittest.main()
