54f1e32ac0a5ee198bf64f51aeb67456b29fe0a2
Mithali Shashidhar <mithalishashidhar@Mithalis-MacBook-Pro.local>
2023-03-13 22:06:32 -0700

    added tanu and ananya changes

--- Tourism_Recommenders/frontend.py
+++ Tourism_Recommenders/frontend.py
@@ -1,41 +1,199 @@
 """
-This module is used to run the frontend
-of our application
+Frontend code for the Weather-Integrated Tourism Recommendation System
+
+This script looks into the frontend of the code where user inputs like
+preference, date and time of travel, preferred minimum rating,
+preferred maximum price level, and location are taken. These variables
+are then used to call the backend module and fetch a list of
+recommendations which are then showed as an output to the user.
+
+The script requires that the 'pandas', 'streamlit' and 'const' packages
+be installed within the Python environment this script is being run in.
+This script also imports the 'backend' module to execute the program.
+
 """
 
+import pandas as pd
 import streamlit as st
-from Tourism_Recommenders import recommender_constants
+from Tourism_Recommenders import const
 from Tourism_Recommenders import backend
 
 
-r = st.number_input("Minimum rating you want 1-5")
-b = st.number_input("Price level 1-5")
-date = st.date_input("When do you wish to visit (only upto 1 day beyond today")
-time = st.time_input("What time do you wish to visit")
-location = st.text_input("Enter your location or leave blank to use your current location")
+st.set_page_config(layout = "wide")
 
-api_key = backend.decode_api_key(recommender_constants.ENCODED_GOOGLE_API_KEY)
-if location == "":
-    location = backend.get_user_entered_loc(location)
+var = st.empty()
 
+#create a container that takes user inputs
+with var.container():
+    st.header(":blue[ANITA: Tourism Recommendation System]")
+    pref = st.selectbox(':violet[Do you have any particular preference for a\
+                        place?]', ('None', 'Beaches', 'Cafes and Restaurants',
+                        'Hikes and Trails', 'Indoor Activities', 'Museums',
+                        'Outdoor Activities', 'Parks and Botanical Gardens'))
+    date = st.date_input(':violet[When do you wish to visit?] (To get more\
+                         accurate results based on weather predictions, please\
+                         enter a date that is not beyond 2-3 days from today.\
+                         Due to the limitations in our weather dataset, dates\
+                         beyond 8 days from today are invalid!)')
+    time = st.time_input(":violet[What time do you wish to visit?]")
+    rating = st.number_input(":violet[Minimum rating you want from 1-5]",
+                            min_value = 0.0, max_value = 5.0, value = 3.5,
+                            step = 0.1)
+    price = st.number_input(":violet[Maximum price level] (From 1-5 with 1\
+                            being the lowest and 5 being the highest)",
+                            min_value = 1, max_value = 5, value = 3, step = 1)
+    location = st.text_input(":violet[Enter your location or leave blank to\
+                             use your current location] (Permissible\
+                             characters are uppercase english letters [A-Z],\
+                             lowercase english letters [a-z], digits [0-9],\
+                             and commas [,])")
+    enter_button = st.button(label='Enter!')
 
-st.write(location)
+var.container()
 
-latitude, longitude = backend.get_latitude_and_longitude(location)
-meteostat_api_response = backend.call_meteostat_api(latitude,longitude,date,time)
-st.write(meteostat_api_response)
+if enter_button:
 
+    var.empty()
+    return_button = st.button(label='Return')
 
-query = backend.basic_rules(meteostat_api_response, location)
-google_api_response = backend.call_google_api(recommender_constants.GOOGLE_MAPS_API
-                                    , query, api_key, 'opening_hours')
-st.write(google_api_response)
+    #check for exceptions
+    backend.check_date_invalid(date, time)
+    backend.check_address_characters(location)
+    backend.check_address_invalid(location)
 
+    if return_button:
+        var.container()
+    else:
+        api_key = backend.decode_api_key(const.ENCODED_GOOGLE_API_KEY)
 
-# ST.WRITE TO THE FRONT END HERE
+        if location.strip() == "":
+            location = backend.get_user_entered_loc()
+        else:
+            pass
 
-loc = st.text_input("Enter your location or leave blank to use your current location")
+        latitude, longitude = backend.get_latitude_and_longitude(location)
+        meteostat_api_response = backend.call_meteostat_api(latitude,
+                                                            longitude, date,
+                                                            time)
 
-res = backend.get_user_entered_loc(loc)
+        queries = backend.basic_rules(meteostat_api_response, location)
 
-st.write(res)
+        PREF = str(pref) + " in " + str(location)
+        personal_pref = [PREF]
+
+        #fetch places based on weather recommendations
+        google_api_response = backend.call_google_api(const.GOOGLE_MAPS_API,
+                                                    queries, api_key,
+                                                    'opening_hours.periods',
+                                                    latitude, longitude,
+                                                    rating, price)
+
+        #fetch places if the user inputs that they have a preference
+        if pref != 'None':
+            pref_df = backend.call_google_api(const.GOOGLE_MAPS_API,
+                                            personal_pref, api_key,
+                                            'opening_hours', latitude,
+                                            longitude, rating, price)
+        else:
+            pref_df = pd.DataFrame()
+
+        #write header based on the weather condition
+        if google_api_response.empty:
+            st.header('Oops! Looks like the weather is not favourable for\
+                      your desired date and location :(')
+        else:
+            if meteostat_api_response == 0:
+                st.header(":blue[The weather for your desired date is\
+                          uncertain. But don't worry! We got you :)]")
+            elif meteostat_api_response <= 2:
+                st.header(':blue[The weather for your desired date is sunny\
+                        :sun_with_face:]')
+            elif meteostat_api_response <= 5:
+                st.header(':blue[The weather for your desired date is cloudy\
+                        :cloud:]')
+            elif meteostat_api_response <= 11 or meteostat_api_response == 17\
+                or meteostat_api_response == 18:
+                st.header(':blue[The weather for your desired date is rainy\
+                        :rain_cloud:]')
+            elif meteostat_api_response in (14, 15, 21):
+                st.header(':blue[The weather for your desired date is snowy\
+                        :snowman:]')
+
+        #output when the user has a particular preference
+        if pref != 'None' and not google_api_response.empty:
+            st.header(':violet[Here are some recommendations based on your\
+                    preference!]')
+            if  'Beaches' in pref:
+                st.subheader(f"""**{pref}** :beach_with_umbrella:""")
+            elif  'Botanical Gardens' in pref:
+                st.subheader(f"""**{pref}** :herb:""")
+            elif  'Cafes' in pref:
+                st.subheader(f"""**{pref}** :cake:""")
+            elif  'Hikes' in pref:
+                st.subheader(f"""**{pref}** :national_park:""")
+            elif  'Indoor Activities' in pref:
+                st.subheader(f"""**{pref}** :house_buildings:""")
+            elif  'Museums' in pref:
+                st.subheader(f"""**{pref}** :classical_building:""")
+            elif  'Outdoor Activities' in pref:
+                st.subheader(f"""**{pref}** :running:""")
+            elif  'Parks' in pref:
+                st.subheader(f"""**{pref}** :deciduous_tree:""")
+            else:
+                st.subheader(f"""**{pref}** :fork_and_knife:""")
+
+            pref_df = pref_df.head(3)
+            for _, row in pref_df.iterrows():
+                st.markdown(f""":violet[{row['name']}]""")
+                st.markdown(f"""Address: {row['formatted_address']}""")
+                st.markdown(f"""Rating: {row['rating']}""")
+                st.markdown(f"""Distance: Approximately {row['Distance']}\
+                        miles away  \n """)
+            st.write("  \n ")
+        else:
+            pass
+
+        #output based on the tool's recommendations
+        if google_api_response.empty is False:
+            st.header(':violet[Here are some recommendations from us!]')
+            for query in queries:
+                if query.split(' in')[0] not in PREF:
+                    if  'Beaches' in query.split(' in')[0]:
+                        st.subheader(f"""**{query.split(' in')[0]}**\
+                            :beach_with_umbrella:""")
+                    elif  'Botanical Gardens' in query.split(' in')[0]:
+                        st.subheader(f"""**{query.split(' in')[0]}** :herb:""")
+                    elif  'Cafes' in query.split(' in')[0]:
+                        st.subheader(f"""**{query.split(' in')[0]}** :cake:""")
+                    elif  'Hikes' in query.split(' in')[0]:
+                        st.subheader(f"""**{query.split(' in')[0]}**\
+                            :national_park:""")
+                    elif  'Indoor Activities' in query.split(' in')[0]:
+                        st.subheader(f"""**{query.split(' in')[0]}**\
+                            :house_buildings:""")
+                    elif  'Museums' in query.split(' in')[0]:
+                        st.subheader(f"""**{query.split(' in')[0]}**\
+                            :classical_building:""")
+                    elif  'Outdoor Activities' in query.split(' in')[0]:
+                        st.subheader(f"""**{query.split(' in')[0]}**\
+                            :running:""")
+                    elif  'Parks' in query.split(' in')[0]:
+                        st.subheader(f"""**{query.split(' in')[0]}**\
+                            :deciduous_tree:""")
+                    else:
+                        st.subheader(f"""**{query.split(' in')[0]}**\
+                            :fork_and_knife:""")
+                else:
+                    continue
+
+                df = google_api_response[google_api_response['Query']==query]
+                df = df.head(3)
+                for _, row in df.iterrows():
+                    st.markdown(f""":violet[{row['name']}]""")
+                    st.markdown(f"""Address: {row['formatted_address']}""")
+                    st.markdown(f"""Rating: {row['rating']}""")
+                    st.markdown(f"""Distance: Approximately {row['Distance']}\
+                          miles away  \n """)
+                st.write("  \n ")
+        else:
+            pass
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,15 +1,129 @@
+'''
+This is the unit test file for the code.
+'''
 import unittest
-from Tourism_Recommenders.backend import *
+from datetime import datetime
+from Tourism_Recommenders.backend import call_google_api, call_meteostat_api, decode_api_key, \
+get_latitude_and_longitude, get_user_entered_loc, basic_rules, check_date_invalid, \
+check_address_invalid, check_address_characters, haversine
+import Tourism_Recommenders.const as const
 
 
 class TestBackend(unittest.TestCase):
-    def test_call_google_api_smoke_test(self):
+    '''
+    Class to conduct unit tests for the backend functions
+    '''
+    def test_call_google_api(self):
+        '''
+        tests the working of the call_google_api function
+        '''
         url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
         query = "Tourist places to visit in Seattle"
         api_key = 'AIzaSyDvXY6zrSDLbP_uNCc7UV1zWqobtkk0l7A'
         fields = 'opening_hours'
-        google_api_response = call_google_api(url, query, api_key, fields)
+        lat = 47.6062
+        longi = 122.3321
+        rating = 3
+        price = 2
+        call_google_api(url, query, api_key, fields, lat, longi, rating, price)
         self.assertTrue(True)
 
+
+    def test_call_meteostat_api(self):
+        '''
+        tests the working of the call_meteostat_api function
+        '''
+        latitude = 47.6062
+        longitude = 122.3321
+        date = "2023-03-12"
+        time = "17:02:00"
+        date_obj = datetime.strptime(date, '%Y-%m-%d').date()
+        time_obj = datetime.strptime(time, '%H:%M:%S').time()
+        call_meteostat_api(latitude, longitude, date_obj, time_obj)
+        self.assertTrue(True)
+
+    def test_decode_api_key(self):
+        '''
+        tests the working of the decode_api_key function
+        '''
+        api_key = const.ENCODED_GOOGLE_API_KEY
+        decode_api_key(api_key)
+        self.assertTrue(True)
+
+
+    def test_get_latitude_and_longitude(self):
+        '''
+        tests the working of the get_latitude_and_longitude function
+        '''
+        location = "Seattle"
+        get_latitude_and_longitude(location)
+        self.assertTrue(True)
+
+    def test_get_user_entered_loc(self):
+        '''
+        tests the working of the get_user_entered_loc function
+        '''
+        get_user_entered_loc()
+        self.assertTrue(True)
+
+    def test_basic_rules(self):
+        '''
+        tests the working of the basic_rules function
+        '''
+        defining_weather = 2
+        location = "Seattle"
+        basic_rules(defining_weather, location)
+        self.assertTrue(True)
+
+    #Edge Tests
+    def test_edge_correct_date(self):
+        """
+        This function performs an edge test to check how the program
+        works when one goes "off script". This test accounts for the
+        case where the entered date and time belongs to a past date or
+        the entered date is beyond 8 days from the current date.
+
+        """
+        date = "2023-03-10"
+        time = "17:02:00"
+        date_obj = datetime.strptime(date, '%Y-%m-%d').date()
+        time_obj = datetime.strptime(time, '%H:%M:%S').time()
+        with self.assertRaises(ValueError):
+            check_date_invalid(date_obj, time_obj)
+
+    def test_edge_address_validity(self):
+        """
+        This function performs an edge test to check how the program
+        works when one goes "off script". This test accounts for the
+        case where the entered address is invalid and does not exist
+        in the google maps api dataset.
+
+        """
+        location_as_string = "Csdgfsdg"
+        with self.assertRaises(ValueError):
+            check_address_invalid(location_as_string)
+
+    def test_edge_address_char_validity(self):
+        """
+        This function performs an edge test to check how the program
+        works when one goes "off script". This test accounts for the
+        case where the entered address has invalid characters i.e.
+        anything other than english letters, digits, or commas.
+        """
+        location_as_string = "2$ Capitol Hill^"
+        with self.assertRaises(ValueError):
+            check_address_characters(location_as_string)
+
+    def check_haversine_fn(self):
+        '''
+        Should return 0 as both the locations are the same.
+        '''
+        lat1 = 47.6062
+        long1 = 122.3321
+        lat2 = 47.6062
+        long2 = 122.3321
+        dist = haversine(lat1, long1, lat2, long2)
+        self.assertAlmostEqual(dist, 0)
+
 if __name__ == "__main__":
-   unittest.main()
+    unittest.main()
